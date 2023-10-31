149a4e2fa4e2ddf25d20808f13851a4b7c172ab2
Sagor <sagor.heaking@gmail.com>
2023-10-31 23:53:41 +0600

    footer section complete

---
+++ src/Components/Home/Footer/Footer.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import{FaFacebook, FaGithub, FaLinkedin} from "react-icons/fa"
+
+const Footer = () => {
+    return (
+        <footer className="footer items-center p-4 border px-10">
+  <aside className="items-center grid-flow-col">
+  <a className="btn normal-case  ">
+          <img
+            className="h-full "
+            src="https://assets-global.website-files.com/6077f96cf4fa19216396daaf/63f36981950c481a7701e95f_logo-p-500.png"
+            alt=""
+          />
+        </a>
+    <p>Copyright © 2023 - All right reserved</p>
+  </aside> 
+  <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+    <a>
+        <FaGithub className='w-6 h-6'/>
+    </a> 
+    <a>
+        <FaLinkedin className='w-6 h-6'/>
+    </a>
+    <a>
+        <FaFacebook className='w-6 h-6'/>
+    </a>
+  </nav>
+</footer>
+    );
+};
+
+export default Footer;
--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,13 +1,15 @@
-import { Outlet } from 'react-router-dom';
-import Navbar from '../Home/Navbar/Navbar';
+import { Outlet } from "react-router-dom";
+import Navbar from "../Home/Navbar/Navbar";
+import Footer from "../Home/Footer/Footer";
 
 const Layout = () => {
-    return (
-        <div>
-        <Navbar></Navbar>
-            <Outlet></Outlet>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Layout;
